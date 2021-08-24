fb6b0ce8bcff0cb9b3ccbc255303782f294cb8e3
HONOKA ANRAKU <rakuraku2563nonnon@gmail.com>
2021-08-24 12:01:46 +0900

    fourhcommit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,9 +13,15 @@ class TaskController extends Controller
         $folders=Folder::all();
 
         //選ばれたフォルダを取得する
+        $current_folder=Folder::find($id);
+
+        //選ばれたフォルダに紐づくタスクを取得する
+        $tasks=$current_folder->tasks()->get();
+
         return view('tasks/index',[
             'folders'=>$folders,
-            'current_folder_id'=>$id,
+            'current_folder_id'=>$current_folder->id,
+            'tasks'=>$tasks,
         ]);
     }
 }
